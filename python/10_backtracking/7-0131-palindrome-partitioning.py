b5d7ca2d7a5e36fb48e9aca258365d68123ea174
Chris Cooper <christopher.p.cooper@gmail.com>
2023-09-16 15:54:12 -0700

    refactor

--- python/10_backtracking/7-0131-palindrome-partitioning.py
+++ python/10_backtracking/7-0131-palindrome-partitioning.py
@@ -4,10 +4,10 @@ class Solution:
         self.dfs(s, [], res)
         return res
 
-    def dfs(self, s: str, path: List[str], res: List[List[str]]):
+    def dfs(self, s: str, palindromes: List[str], res: List[List[str]]):
         if not s:
-            res.append(path)
+            res.append(palindromes)
         else:
             for i in range(1, len(s) + 1):
                 if s[:i] == s[:i][::-1]:
-                    self.dfs(s[i:], path + [s[:i]], res)
+                    self.dfs(s[i:], palindromes + [s[:i]], res)
