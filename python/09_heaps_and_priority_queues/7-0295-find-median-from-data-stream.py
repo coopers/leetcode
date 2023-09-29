7c3fed1a01028125ed153aacb145319d5b5d800e
Chris Cooper <christopher.p.cooper@gmail.com>
2023-09-29 13:44:48 -0700

    refactor

--- python/09_heaps_and_priority_queues/7-0295-find-median-from-data-stream.py
+++ python/09_heaps_and_priority_queues/7-0295-find-median-from-data-stream.py
@@ -3,26 +3,24 @@ import heapq
 
 class MedianFinder:
     def __init__(self):
-        """
-        initialize your data structure here.
-        """
-        # Two heaps of equal size
         self.lil, self.big = [], []  # maxHeap, minHeap
 
     def addNum(self, num: int) -> None:
-        if self.lil and num < -self.lil[0]:
-            heapq.heappush(self.lil, -num)
+        if self.lil and num < self.lil[0]:
+            self.lil.append(num)
+            heapq._siftdown_max(self.lil, 0, len(self.lil) - 1)
         else:
             heapq.heappush(self.big, num)
 
         if len(self.lil) > len(self.big) + 1:
-            heapq.heappush(self.big, -heapq.heappop(self.lil))
+            heapq.heappush(self.big, heapq._heappop_max(self.lil))
         elif len(self.big) > len(self.lil) + 1:
-            heapq.heappush(self.lil, -heapq.heappop(self.big))
+            self.lil.append(heapq.heappop(self.big))
+            heapq._siftdown_max(self.lil, 0, len(self.lil) - 1)
 
     def findMedian(self) -> float:
         if len(self.lil) > len(self.big):
-            return -self.lil[0]
+            return self.lil[0]
         elif len(self.big) > len(self.lil):
             return self.big[0]
-        return (-self.lil[0] + self.big[0]) / 2.0
+        return (self.lil[0] + self.big[0]) / 2.0
