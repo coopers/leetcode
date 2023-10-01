53f53125dc7a51ae5fbf49ac0176536a09482e56
Chris Cooper <christopher.p.cooper@gmail.com>
2023-09-30 19:11:02 -0700

    refactor

--- python/11_graphs/02-0133-clone-graph.py
+++ python/11_graphs/02-0133-clone-graph.py
@@ -1,22 +1,20 @@
-# Definition for a Node.
 class Node:
     def __init__(self, val = 0, neighbors = None):
         self.val = val
         self.neighbors = neighbors if neighbors is not None else []
 
-
 class Solution:
     def cloneGraph(self, node: Node) -> Node:
-        oldToNew = {}
+        nodeToClone = {}
 
         def dfs(node):
-            if node in oldToNew:
-                return oldToNew[node]
+            if node in nodeToClone:
+                return nodeToClone[node]
 
-            copy = Node(node.val)
-            oldToNew[node] = copy
-            for nei in node.neighbors:
-                copy.neighbors.append(dfs(nei))
-            return copy
+            clone = Node(node.val)
+            nodeToClone[node] = clone
+            for neighbor in node.neighbors:
+                clone.neighbors.append(dfs(neighbor))
+            return clone
 
         return dfs(node) if node else None
--- python/11_graphs/04-0417-pacific-atlantic-water-flow.py
+++ python/11_graphs/04-0417-pacific-atlantic-water-flow.py
@@ -4,23 +4,21 @@ from typing import List
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
         ROWS, COLS = len(heights), len(heights[0])
-        pac, atl = set(), set()
+        pac = [[False] * COLS for _ in range(ROWS)]
+        atl = [[False] * COLS for _ in range(ROWS)]
 
         def dfs(r, c, visit, prevHeight):
             if (
-                (r, c) in visit
-                or r < 0
-                or c < 0
-                or r == ROWS
-                or c == COLS
-                or heights[r][c] < prevHeight
+                0 <= r < ROWS and
+                0 <= c < COLS and
+                not visit[r][c] and
+                heights[r][c] >= prevHeight
             ):
-                return
-            visit.add((r, c))
-            dfs(r + 1, c, visit, heights[r][c])
-            dfs(r - 1, c, visit, heights[r][c])
-            dfs(r, c + 1, visit, heights[r][c])
-            dfs(r, c - 1, visit, heights[r][c])
+                visit[r][c] = True
+                dfs(r + 1, c, visit, heights[r][c])
+                dfs(r - 1, c, visit, heights[r][c])
+                dfs(r, c + 1, visit, heights[r][c])
+                dfs(r, c - 1, visit, heights[r][c])
 
         for c in range(COLS):
             dfs(0, c, pac, heights[0][c])
@@ -30,9 +28,5 @@ class Solution:
             dfs(r, 0, pac, heights[r][0])
             dfs(r, COLS - 1, atl, heights[r][COLS - 1])
 
-        res = []
-        for r in range(ROWS):
-            for c in range(COLS):
-                if (r, c) in pac and (r, c) in atl:
-                    res.append([r, c])
-        return res
+        return [(r, c) for r in range(ROWS) for c in range(COLS) 
+                if pac[r][c] and atl[r][c]]
