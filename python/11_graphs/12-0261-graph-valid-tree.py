ec582a27ad737d6635373e71bd96028f851bf4ed
Chris Cooper <christopher.p.cooper@gmail.com>
2023-10-02 14:27:33 -0700

    refactor

--- python/11_graphs/12-0261-graph-valid-tree.py
+++ python/11_graphs/12-0261-graph-valid-tree.py
@@ -1,76 +1,55 @@
 from typing import List
 
 
-# Problem is free on Lintcode
+# Union-Find
 class Solution:
-    """
-    @param n: An integer
-    @param edges: a list of undirected edges
-    @return: true if it's a valid tree, or false
-    """
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1:
+            return False
+        
+        parent = [i for i in range(n)]
+        rank = [1] * n
 
-    def validTree(self, n, edges):
-        if not n:
-            return True
-        adj = {i: [] for i in range(n)}
-        for n1, n2 in edges:
-            adj[n1].append(n2)
-            adj[n2].append(n1)
+        def find(n):
+            while n != parent[n]:
+                n, parent[n] = parent[n], parent[parent[n]]
+            return n
 
-        visit = set()
-
-        def dfs(i, prev):
-            if i in visit:
+        def union(n1, n2):
+            p1, p2 = find(n1), find(n2)
+            if p1 == p2:
                 return False
 
-            visit.add(i)
-            for j in adj[i]:
-                if j == prev:
-                    continue
-                if not dfs(j, i):
-                    return False
+            child, p = (p1, p2) if rank[p1] < rank[p2] else (p2, p1)
+            parent[child] = p
+            rank[p] += rank[child]
             return True
 
-        return dfs(0, -1) and n == len(visit)
-    
-    
-    
-    # alternative solution via DSU O(ElogV) time complexity and 
-    # save some space as we don't recreate graph\tree into adjacency list prior dfs and loop over the edge list directly
-    class Solution:
-    """
-    @param n: An integer
-    @param edges: a list of undirected edges
-    @return: true if it's a valid tree, or false
-    """
-    def __find(self, n: int) -> int:
-        while n != self.parents.get(n, n):
-            n = self.parents.get(n, n)
-        return n
+        return all(union(a, b) for a, b in edges)
 
-    def __connect(self, n: int, m: int) -> None:
-        pn = self.__find(n)
-        pm = self.__find(m)
-        if pn == pm:
-            return
-        if self.heights.get(pn, 1) > self.heights.get(pm, 1):
-            self.parents[pn] = pm
-        else:
-            self.parents[pm] = pn
-            self.heights[pm] = self.heights.get(pn, 1) + 1
-        self.components -= 1
 
-    def valid_tree(self, n: int, edges: List[List[int]]) -> bool:
-        # init here as not sure that ctor will be re-invoked in different tests
-        self.parents = {}
-        self.heights = {}
-        self.components = n
-
-        for e1, e2 in edges:
-            if self.__find(e1) == self.__find(e2):  # 'redundant' edge
+# DFS
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) != n - 1:
+            return False
+        
+        neighbors = [[] for _ in range(n)]
+        for a, b in edges:
+            neighbors[a].append(b)
+            neighbors[b].append(a)
+        
+        visit = [False] * n
+        def dfs(i):
+            if visit[i]:
                 return False
-            self.__connect(e1, e2)
 
-        return self.components == 1  # forest contains one tree
+            visit[i] = True
+            for j in neighbors[i]:
+                dfs(j)
+            return True
 
-
+        if not dfs(0):
+            return False
+        
+        return all(visit[i] for i in range(n))
