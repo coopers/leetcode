f3cd252cbef5c3322a9c703aaec709c1fc299b4c
Chris Cooper <christopher.p.cooper@gmail.com>
2023-12-04 16:03:40 -0800

    refactor

--- python/18_bit_manipulation/04-0190-reverse-bits.py
+++ python/18_bit_manipulation/04-0190-reverse-bits.py
@@ -1,7 +1,11 @@
+NUM_BITS = 32
 class Solution:
     def reverseBits(self, n: int) -> int:
         res = 0
-        for i in range(32):
-            bit = (n >> i) & 1
-            res += (bit << (31 - i))
+        for _ in range(NUM_BITS):
+            res <<= 1
+            if n & 1:
+                res += 1
+            n >>= 1
+
         return res
